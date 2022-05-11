a7c1718982666a058727a72d51d70abbd0aa4cd9
Nicknameee <igrunka1991@gmail.com>
2022-05-12 02:11:57 +0300

    .

--- src/main/java/application/web/configuration/ApplicationSecurityConfiguration.java
+++ src/main/java/application/web/configuration/ApplicationSecurityConfiguration.java
@@ -48,7 +48,8 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .requiresChannel()
-                .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+                //.requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+                .anyRequest()
                 .requiresSecure();
         http
                 .csrf().disable();
