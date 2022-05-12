8a0d675ee430c0053db0f4a014a6067d84ff4f40
Nicknameee <igrunka1991@gmail.com>
2022-05-12 16:35:50 +0300

    Refactoring

--- src/main/java/application/web/controllers/UserController.java
+++ src/main/java/application/web/controllers/UserController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/api/user")
-@CrossOrigin(origins = "*")
 public class UserController {
     private final Logger logger = LoggerFactory.getLogger(UserController.class);
 
