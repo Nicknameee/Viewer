ed8790bcc9822787f26ebd2ed6d96caa575f7d0e
Nicknameee <igrunka1991@gmail.com>
2022-05-19 02:29:09 +0300

    Language logic for registered users

--- target/classes/static/manager/scripts/v2/basicAdmin.js
+++ target/classes/static/manager/scripts/v2/basicAdmin.js
@@ -1,3 +1,29 @@
 function showChangesAlert() {
     $("#changes-alert").removeClass('d-none')
 }
+function changeLanguage(element) {
+    $.ajax({
+        url: '/api/user/update/language',
+        type: 'PUT',
+        data: {
+            mail: $("#email").text(),
+            language: $(element).text()
+        },
+        success:
+            function (response) {
+                if (response.success) {
+                    location.pathname = '/api/user/personal'
+                }
+                else {
+                    alert("Error occurs while language changing")
+                    console.log(response.error)
+                }
+            },
+        error:
+            function (response) {
+                alert("Error occurs while language changing")
+                console.log(response.error)
+            }
+    })
+
+}
